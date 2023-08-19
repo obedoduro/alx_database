95f48f2fed17fca0e0d07891d3b3df1b4f8b1480
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-08-19 21:11:13 +0000

    first add

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -7,4 +7,5 @@ INSERT INTO first_table (id, name)
 VALUES (89, 'Holberton School');
 
 -- Print the inserted row
-SELECT * FROM first_table WHERE id = 89;
+--SELECT * FROM first_table WHERE id = 89;
+PRINT first_table;
