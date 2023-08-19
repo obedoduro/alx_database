95bd69576e9f80a33896a2bded0463dcae6fee7b
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-08-19 20:53:27 +0000

    table in database

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,12 @@
+--create TABLE IN A CURRENT DATABASE
+
+USE mysql
+
+--create tables
+CREATE TABLE IF NOT EXISTS first_table(
+    id INT,
+    name VARCHAR(256)
+);
+
+--DOES NOT FAIL IF TABLE EXIST
+DO NOTHING;
