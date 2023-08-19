1cd434a1b21177c1e4cc0753a27c929dac708bca
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-08-19 21:14:41 +0000

    count

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,11 +1,10 @@
 -- Save this script as 7-insert_value.sql
 
-USE first_table;
+USE hbtn_0c_0;
 
 -- Insert the new row into the first_table
 INSERT INTO first_table (id, name)
 VALUES (89, 'Holberton School');
 
 -- Print the inserted row
---SELECT * FROM first_table WHERE id = 89;
-PRINT first_table;
+SELECT * FROM first_table WHERE id = 89;
