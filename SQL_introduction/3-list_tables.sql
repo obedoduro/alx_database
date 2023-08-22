ba6a9c2c9a66e085624680a7beda6a0b2a916e63
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-08-22 13:43:04 +0000

    list

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,12 +1,6 @@
--- Create the database if it doesn't exist
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
-
--- Use the database
+--create TABLE IN A CURRENT DATABASE
 USE hbtn_0c_0;
-
-
--- Create the table if it doesn't exist
-CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
-
--- Create the table if it doesn't exist
-CALL ListTablesInDatabase('hbtn_0c_0');
+--LIST TABLES
+SELECT TABLE_NAME
+FROM INFORMATION_SCHEMA.TABLES
+WHERE TABLE_SCHEMA = 'hbtn_0c_0'; -- Replace with the actual database name
