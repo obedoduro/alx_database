51e700027acc96104a23c3db9a66e81bf5b97118
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-08-22 13:21:11 +0000

    list

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,10 +1,2 @@
 
-
--- Creating database if it doesn't exist
-mysql -u your_username -p -e "CREATE DATABASE IF NOT EXISTS hbtn_0c_0;"
-
--- Storing the database name in a variable
-hbtn_0c_0="hbtn_0c_0"
-
--- MySQL command to list tables in the given database
-mysql -u your_username -p -e "USE $hbtn_0c_0; SHOW TABLES;"
+USE hbtn_0c_0;
