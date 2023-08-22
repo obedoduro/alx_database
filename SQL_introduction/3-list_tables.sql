d291cee7b89238b662180f335c97493a6eb5e8b4
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-08-22 11:11:30 +0000

    list

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,13 +1,13 @@
 #!/bin/bash
 
--- Check if the database name is provided as an argument
-if [ $# -eq 0 ]; then
-    echo "Please provide the database name as an argument."
-    exit 1
-fi
 
 -- Store the database name from the argument
-database_name="$1"
+database_name="
+holbteron_0
+holbteron_1
+holbteron_2
+Tables_in_hbtn_test_db_0
+"
 
 -- MySQL command to list tables in the given database
 mysql -u your_username -p -e "USE $database_name; SHOW TABLES;"
