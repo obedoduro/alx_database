f86f161bccaa541222530f53a2ace096b3d42bc8
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-08-22 11:13:40 +0000

    list

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,8 +1,9 @@
-#!/bin/bash
 
+-- creating database, if there is one script should not fail
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 
 -- Store the database name from the argument
-database_name="
+hbtn_0c_0="
 holbteron_0
 holbteron_1
 holbteron_2
@@ -10,4 +11,4 @@ Tables_in_hbtn_test_db_0
 "
 
 -- MySQL command to list tables in the given database
-mysql -u your_username -p -e "USE $database_name; SHOW TABLES;"
+mysql -u your_username -p -e "USE $hbtn_0c_0; SHOW TABLES;"
