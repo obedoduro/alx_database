27b55c3ff725d67525403e7332732ad1108554ff
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-08-22 13:04:39 +0000

    list

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,8 +1,8 @@
 -- Create the database if it doesn't exist
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+--CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 
 -- Use the database
-USE hbtn_0c_0;
+--USE hbtn_0c_0;
 
 -- Create the table if it doesn't exist
-CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
+--CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
