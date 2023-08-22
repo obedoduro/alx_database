9eb4257bc2e73d2227056ecafa509f466b6c1d13
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-08-22 13:46:13 +0000

    list

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,5 +1,9 @@
---create TABLE IN A CURRENT DATABASE
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+
+-- Use the database
 USE hbtn_0c_0;
+
 --LIST TABLES
 SELECT TABLE_NAME
 FROM INFORMATION_SCHEMA.TABLES
