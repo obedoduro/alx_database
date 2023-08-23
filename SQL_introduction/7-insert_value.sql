dba3c0564b40d3584a6d0bd8dfee68fb587c6d53
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-08-23 15:50:16 +0000

    list all tables

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,10 +1,31 @@
--- Save this script as 7-insert_value.sql
+-- Create the database if it doesn't exist
+CREATE DATABASE
+IF NOT EXISTS hbtn_0c_0;
 
+
+
+-- Save this script as list_first_table_rows.sql
 USE hbtn_0c_0;
 
--- Insert the new row into the first_table
-INSERT INTO first_table (id, name)
-VALUES (89, 'Holberton School');
 
--- Print the inserted row
-SELECT * FROM first_table WHERE id = 89;
+-- Create the table if it doesn't exist
+CREATE TABLE
+IF NOT EXISTS first_table
+(
+  id int ,
+  name varchar
+(128) 
+ );
+
+
+-- INSERT NEW FIELD INTO TABLE
+INSERT INTO first_table
+    (id, name)
+VALUES
+    (89, 'Holberton School');
+
+
+-- fetch the row of the table
+SELECT *
+FROM first_table;
+
