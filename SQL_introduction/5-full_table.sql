76cf49b81aed37b4ec441eeea6cd9763a575885c
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-08-23 14:47:41 +0000

    describtion

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -8,19 +8,12 @@ IF NOT EXISTS hbtn_0c_0;
 USE hbtn_0c_0;
 
 -- Create the table if it doesn't exist
-CREATE TABLE first_table(
-'CREATE TABLE `first_table`
-(
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar
-(128) DEFAULT NULL,
-  `c` char
-(1) DEFAULT NULL,
-  `created_at` date DEFAULT NULL,
-  PRIMARY KEY
-(`id`)
+CREATE TABLE first_table (
+  id int NOT NULL AUTO_INCREMENT,
+  name varchar(128) DEFAULT NULL,
+  c char(1) DEFAULT NULL,
+  created_at date DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-)'
-)
+
 DESC first_table
-SHOW first_table
--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,12 +1,13 @@
--- Save this script as list_first_table_rows.sql
+-- Create the database if it doesn't exist
+CREATE DATABASE
+IF NOT EXISTS hbtn_0c_0;
 
+
+
+-- Save this script as list_first_table_rows.sql
 USE hbtn_0c_0;
 
--- Create a temporary table to store the results
-CREATE TEMPORARY TABLE tmp_first_table_rows AS SELECT * FROM first_table;
 
--- Show the data from the temporary table
-SELECT * FROM tmp_first_table_rows;
-
--- Drop the temporary table
-DROP TEMPORARY TABLE IF EXISTS tmp_first_table_rows;
+--fecth the row of the table
+SELECT *
+FROM first_table;
