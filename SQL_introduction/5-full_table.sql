330e56692ef802c475a32a33d8d35123e3d8f413
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-08-23 13:40:42 +0000

    list

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,24 +1,14 @@
--- Save this script as 5-full_table.sql
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 
-USE hbtn_0c_0;
 
--- Create a temporary table to capture the table structure
-CREATE TABLE IF NOT EXISTS tmp_table_structure (
-    structure VARCHAR(1000)
-);
 
--- Insert the table structure into the temporary table
-INSERT INTO tmp_table_structure (structure)
-VALUES (
-    CONCAT('CREATE TABLE `first_table` (', 
-           (SELECT GROUP_CONCAT(COLUMN_NAME, ' ', COLUMN_TYPE) 
-            FROM information_schema.COLUMNS 
-            WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table'),
-           ') ENGINE=InnoDB DEFAULT CHARSET=latin1')
-);
+-- Use thedatabase
+ USE hbtn_0c_0;
 
--- Print the table structure
-SELECT structure FROM tmp_table_structure;
-
--- Drop the temporary table
-DROP TABLE IF EXISTS tmp_table_structure;
+-- Create the table if it doesn't exist
+CREATE TABLE
+IF NOT EXISTS first_table
+(id INT, name VARCHAR
+(256));
+SHOW tables;
