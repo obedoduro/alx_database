97861f7a03ce5fb6aef8bfc879269e9e85ec09e8
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-08-23 20:31:45 +0000

    list all tables

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -8,12 +8,9 @@ IF NOT EXISTS hbtn_0c_0;
 USE hbtn_0c_0;
 
 -- Create the table if it doesn't exist
-CREATE TABLE first_table (
-  id int NOT NULL AUTO_INCREMENT,
-  name varchar(128) DEFAULT NULL,
-  c char(1) DEFAULT NULL,
-  created_at date DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE
+IF NOT EXISTS first_table
+(id INT);
 
+-- SHOW tables;
 DESC first_table
