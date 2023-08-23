9795052ad0658e43632485781d30c95b3ca83ab8
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-08-23 13:45:40 +0000

    list

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,14 +1,17 @@
 -- Create the database if it doesn't exist
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+CREATE DATABASE
+IF NOT EXISTS hbtn_0c_0;
 
 
 
 -- Use thedatabase
- USE hbtn_0c_0;
+USE hbtn_0c_0;
 
 -- Create the table if it doesn't exist
 CREATE TABLE
 IF NOT EXISTS first_table
 (id INT, name VARCHAR
 (256));
-SHOW tables;
+
+-- Create the database if it doesn't exist
+DESCRIBE first_table;
