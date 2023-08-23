90ad6c3d3fb1d551bd2b32736b9592a43b71e292
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-08-23 12:09:05 +0000

    list

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -4,6 +4,14 @@ CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 -- Use the database
 USE hbtn_0c_0;
 
+CREATE TABLE PersonKs
+(
+    holbteron_0 varchar(255),
+    holbteron_1 varchar(255),
+    holbteron_2 varchar(255),
+    Tables_in_hbtn_test_db_0 varchar(255)
+);
+
 -- LIST TABLES
 
 SHOW tables;
