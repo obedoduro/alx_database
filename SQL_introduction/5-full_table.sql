2c1359d85f04d38c688e74863aedf08faf8d7d00
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-08-23 14:17:02 +0000

    describtion

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -11,14 +11,14 @@ USE hbtn_0c_0;
 CREATE TABLE
 IF NOT EXISTS first_table
 (
-  id int NOT NULL AUTO_INCREMENT,
-  name varchar
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar
 (128) DEFAULT NULL,
-  c char
+  `c` char
 (1) DEFAULT NULL,
-  created_at date DEFAULT NULL,
+  `created_at` date DEFAULT NULL,
   PRIMARY KEY
-(id)
+(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
  DESC first_table;
