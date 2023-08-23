c13bb7c4d55a7435dc67fcd5901a6320b1d132c0
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-08-23 14:02:03 +0000

    describtion

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -10,8 +10,23 @@ USE hbtn_0c_0;
 -- Create the table if it doesn't exist
 CREATE TABLE
 IF NOT EXISTS first_table
-(id INT, name VARCHAR
-(256));
+(name
+varchar
+(256)
+);
+SELECT
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_DEFAULT,
+    CASE
+        WHEN COLUMN_KEY = 'PRI' THEN 'PRI'
+        ELSE ''
+    END AS COLUMN_KEY,
+    EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'hbtn_0c_0'
+    AND TABLE_NAME = 'first_table';
 
 -- Create the database if it doesn't exist
-DESC first_table;
+-- DESC first_table;
