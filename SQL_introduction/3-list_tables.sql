4d90d8c1141c75cde91474c214ebf7f430674c9c
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-08-23 11:11:07 +0000

    list

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -4,7 +4,3 @@ CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 -- Use the database
 USE hbtn_0c_0;
 
---LIST TABLES
--- SELECT TABLE_NAME
--- FROM INFORMATION_SCHEMA.TABLES
--- WHERE TABLE_SCHEMA = 'hbtn_0c_0';  -- Replace with the actual database name
