3b937d7d4c05cd5866b67847847c847b156a9c14
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-08-23 12:27:34 +0000

    list

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -4,25 +4,25 @@ CREATE DATABASE IF NOT EXISTS hbtn_test_db_0;
 -- Use the database
 USE hbtn_test_db_0;
 
-CREATE TABLE holbteron_0
+CREATE TABLE IF NOT EXISTS holbteron_0
 (
 
     holbteron_0 varchar(255)
 
 );
-CREATE TABLE holbteron_1
+CREATE TABLE IF NOT EXISTS holbteron_1
 (
 
     holbteron_1 varchar(255)
 
 );
-CREATE TABLE holbteron_2
+CREATE TABLE IF NOT EXISTS holbteron_2
 (
 
     -- holbteron_2 varchar(255)
     Tables_in_hbtn_test_db_0 varchar(255)
 );
-CREATE TABLE Tables_in_hbtn_test_db_0
+CREATE TABLE IF NOT EXISTS Tables_in_hbtn_test_db_0
 (
 
     -- holbteron_2 varchar(255)
