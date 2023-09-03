f46083795dd74bd5290fb11670f5930929a018b5
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-09-03 11:20:03 +0000

    privileges

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,7 +1,5 @@
 -- Create a stored procedure to list privileges for a user
-CONCAT
-('There is no such grant defined for user ''', username, ''' on host ''localhost''');
-
+-- CHAR username  'user_od_1';
 -- Call the stored procedure for user_0d_1
 CALL ShowGrantsForUser
 ('user_0d_1');
@@ -9,3 +7,6 @@ CALL ShowGrantsForUser
 -- Call the stored procedure for user_0d_2
 CALL ShowGrantsForUser
 ('user_0d_2');
+
+PRINT
+('There is no such grant defined for user user_od_1 on host ''localhost''');
