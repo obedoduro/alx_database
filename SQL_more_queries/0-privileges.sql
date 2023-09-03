e374d36adce5082f8ef47f9b6083811eacb36ebd
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-09-03 10:51:54 +0000

    privileges

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,17 @@
 -- Create a stored procedure to list privileges for a user
+DELIMITER
+//
+
+CREATE PROCEDURE ListUserPrivileges(IN username VARCHAR
+(50))
+BEGIN
+    SET @sql = CONCAT('SHOW GRANTS FOR ''', username, '''@''localhost'';');
+    PREPARE stmt FROM @sql;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+END//
+
+DELIMITER ;
 
 -- Call the stored procedure for user_0d_1
 CALL ListUserPrivileges
