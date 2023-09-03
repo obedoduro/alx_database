5b12123a539c41ff8321937ce1684db7f2724649
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-09-03 11:46:12 +0000

    priveleges

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -9,6 +9,20 @@ USE hbtn_0c_0;
 
 CREATE PROCEDURE ShowGrantsForUser(IN username VARCHAR
 (50))
+BEGIN
+    DECLARE done INT DEFAULT FALSE;
+    DECLARE grant_stmt VARCHAR
+    (255);
+DECLARE cur CURSOR FOR
+        SELECT DISTINCT
+    CONCAT('SHOW GRANTS FOR ''', user, '''@''', host, ''';')
+FROM
+    mysql.user
+WHERE
+            user = username;
+
+END
+
 
 -- Call the stored procedure for user_0d_1
 CALL ShowGrantsForUser
