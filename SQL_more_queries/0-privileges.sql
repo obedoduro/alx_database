5a6e73496c521252e77ab2e72f94ffa7e6a914f1
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-09-03 11:11:28 +0000

    privileges

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -2,21 +2,58 @@
 DELIMITER
 
 
-CREATE PROCEDURE ListUserPrivileges(IN username VARCHAR
+CREATE PROCEDURE ShowGrantsForUser(IN username VARCHAR
 (50))
 BEGIN
-    SET @sql = CONCAT('SHOW GRANTS FOR ''', username, '''@''localhost'';');
-    PREPARE stmt FROM @sql;
+    DECLARE done INT DEFAULT FALSE;
+DECLARE grant_stmt VARCHAR
+(255);
+DECLARE cur CURSOR FOR
+        SELECT DISTINCT
+    CONCAT('SHOW GRANTS FOR ''', user, '''@''', host, ''';')
+FROM
+    mysql.user
+WHERE
+            user = username;
+
+-- If no rows are found, display a custom message
+DECLARE
+CONTINUE
+HANDLER FOR NOT FOUND
+SET done
+= TRUE;
+
+OPEN cur;
+read_loop:
+LOOP
+FETCH cur
+INTO grant_stmt;
+IF done THEN
+            LEAVE read_loop;
+END
+IF;
+        PREPARE stmt FROM grant_stmt;
 EXECUTE stmt;
 DEALLOCATE PREPARE stmt;
+END LOOP;
+CLOSE cur;
+
+IF NOT EXISTS (
+        SELECT 1
+FROM mysql.user
+WHERE user = username
+    ) THEN
+SELECT CONCAT('There is no such grant defined for user ''', username, ''' on host ''localhost''');
+END
+IF;
 END
 
 DELIMITER ;
 
 -- Call the stored procedure for user_0d_1
-CALL ListUserPrivileges
+CALL ShowGrantsForUser
 ('user_0d_1');
 
 -- Call the stored procedure for user_0d_2
-CALL ListUserPrivileges
+CALL ShowGrantsForUser
 ('user_0d_2');
