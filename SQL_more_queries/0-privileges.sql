07c2367b7ce4aa01fec4ded6f937c50f97c2fd8c
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-09-03 11:23:15 +0000

    privileges

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,8 @@
 -- Create a stored procedure to list privileges for a user
 -- CHAR username  'user_od_1';
+
+--select server
+USE localhost;
 -- Call the stored procedure for user_0d_1
 CALL ShowGrantsForUser
 ('user_0d_1');
