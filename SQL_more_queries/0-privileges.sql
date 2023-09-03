e931951fb3f017ebc9f521cedd0c2ac92d01f7d3
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-09-03 11:51:53 +0000

    priveleges

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -7,27 +7,28 @@ USE hbtn_0c_0;
 
 -- Create the stored procedure to list privileges for a user
 
-CREATE PROCEDURE ShowGrantsForUser(IN username VARCHAR
-(50))
-BEGIN
-    DECLARE done INT DEFAULT FALSE;
-    DECLARE grant_stmt VARCHAR
-    (255);
-DECLARE cur CURSOR FOR
-        SELECT DISTINCT
-    CONCAT('SHOW GRANTS FOR ''', user, '''@''', host, ''';')
-FROM
-    mysql.user
-WHERE
-            user = username;
+-- CREATE PROCEDURE ShowGrantsForUser(IN username VARCHAR
+-- (50))
+-- BEGIN
+--     DECLARE done INT DEFAULT FALSE;
+--     DECLARE grant_stmt VARCHAR
+--     (255);
+-- DECLARE cur CURSOR FOR
+--         SELECT DISTINCT
+--     CONCAT('SHOW GRANTS FOR ''', user, '''@''', host, ''';')
+-- FROM
+--     mysql.user
+-- WHERE
+--             user = username;
 
-END
+-- END
 
 
--- Call the stored procedure for user_0d_1
-CALL ShowGrantsForUser
-('user_0d_1');
+-- -- Call the stored procedure for user_0d_1
+-- CALL ShowGrantsForUser
+-- ('user_0d_1');
 
--- Call the stored procedure for user_0d_2
-CALL ShowGrantsForUser
-('user_0d_2');
+-- -- Call the stored procedure for user_0d_2
+-- CALL ShowGrantsForUser
+-- ('user_0d_2');
+SHOW PRIVILEGES;
