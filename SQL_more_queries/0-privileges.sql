2f3b32e57c1599454a3571412f52b5bedbe7dc45
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-09-03 10:50:23 +0000

    privileges

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,9 @@
+-- Create a stored procedure to list privileges for a user
+
+-- Call the stored procedure for user_0d_1
+CALL ListUserPrivileges
+('user_0d_1');
+
+-- Call the stored procedure for user_0d_2
+CALL ListUserPrivileges
+('user_0d_2');
