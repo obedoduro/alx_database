1a02b9979ffed924b03b5e8fadaea5e428ff7327
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-09-05 09:14:49 +0000

    root user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,46 +1,58 @@
--- Reset the password for 'user_0d_1' (assuming you have root privileges)
--- SET PASSWORD
--- = 'user_0d_1_pwd';
+-- -- Reset the password for 'user_0d_1' (assuming you have root privileges)
+-- -- SET PASSWORD
+-- -- = 'user_0d_1_pwd';
 
--- CREATE USER
--- IF NOT EXISTS  'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd' ;
+-- -- CREATE USER
+-- -- IF NOT EXISTS  'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd' ;
 
 
--- Grant all privileges to 'user_0d_1' (if not already granted)
+-- -- Grant all privileges to 'user_0d_1' (if not already granted)
+-- -- GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'
+-- -- WITH
+-- -- GRANT OPTION;
+-- -- FLUSH PRIVILEGES;
+
+
+-- -- SELECT user
+-- -- FROM mysql.user
+-- -- WHERE user = 'user_0d_1' AND host = 'localhost';
+
+
+-- -- Verify if the user 'user_0d_1' exists
+-- -- SELECT user
+-- -- FROM mysql.user
+-- -- WHERE user = 'user_0d_1' AND host = 'localhost';
+
+
+-- -- Grant all privileges to 'user_0d_1' on all databases
 -- GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'
 -- WITH
 -- GRANT OPTION;
+
+-- -- If the user does not exist, create it with all privileges
+-- CREATE USER
+-- IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+
+-- -- Set the password for 'user_0d_1' (ensure it matches the one provided)
+-- SET PASSWORD
+-- FOR 'user_0d_1'@'localhost' = PASSWORD
+-- ('user_0d_1_pwd');
+
+
+-- -- Reload privileges to apply changes
 -- FLUSH PRIVILEGES;
 
 
--- SELECT user
--- FROM mysql.user
--- WHERE user = 'user_0d_1' AND host = 'localhost';
+--not tested
 
 
--- Verify if the user 'user_0d_1' exists
--- SELECT user
--- FROM mysql.user
--- WHERE user = 'user_0d_1' AND host = 'localhost';
-
-
--- Grant all privileges to 'user_0d_1' on all databases
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'
-WITH
-GRANT OPTION;
-
--- If the user does not exist, create it with all privileges
+-- Create the user if it doesn't exist
 CREATE USER
-IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+IF NOT EXISTS 'user_0d_1'@'%' IDENTIFIED BY 'user_0d_1_pwd';
 
+-- Grant all privileges to the user on all databases and tables
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'%';
 
--- Set the password for 'user_0d_1' (ensure it matches the one provided)
-SET PASSWORD
-FOR 'user_0d_1'@'localhost' = PASSWORD
-('user_0d_1_pwd');
-
-
--- Reload privileges to apply changes
+-- Flush privileges to apply the changes
 FLUSH PRIVILEGES;
-
-
