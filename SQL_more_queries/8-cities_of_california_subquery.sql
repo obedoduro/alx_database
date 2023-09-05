5bf1e5b6ea38418a4a3268103b384d41c428c1f1
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-09-05 15:50:27 +0000

    cities of califonia

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -4,6 +4,20 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- USE database
 USE hbtn_0d_usa;
 
+-- Create table
+CREATE TABLE
+IF NOT EXISTS cities
+(
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    state_id INT NOT NULL,
+    FOREIGN KEY
+(state_id) REFERENCES states
+(id),
+    name VARCHAR
+(256) NOT NULL
+  
+);
+
 SELECT cities.id, cities.name
 FROM cities
 WHERE cities.state_id = (SELECT id
