a84bbd4e71c2510504a50ecd3a42d7fe66d660e3
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-09-05 18:27:24 +0000

    cities by status

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -5,6 +5,30 @@ IF NOT EXISTS hbtn_0d_usa;
 -- Switch to the database
 USE hbtn_0d_usa;
 
+
+-- Create the 'states' table
+CREATE TABLE
+IF NOT EXISTS states
+(
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    name VARCHAR
+(256) NOT NULL
+);
+
+-- Create the 'cities' table with a foreign key reference to 'states'
+CREATE TABLE
+IF NOT EXISTS cities
+(
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    state_id INT NOT NULL,
+    name VARCHAR
+(256) NOT NULL,
+    FOREIGN KEY
+(state_id) REFERENCES states
+(id)
+);
+
+-- fetch from database
 SELECT cities.id, cities.name, states.name AS state_name
 FROM cities
     JOIN states ON cities.state_id = states.id
