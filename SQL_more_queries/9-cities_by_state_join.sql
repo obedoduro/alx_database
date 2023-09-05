b3942f30fd6d1c3cd13687af0e5a6c51d86869c2
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-09-05 20:38:26 +0000

    cities of califonia

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -104,7 +104,7 @@ VALUES
 SELECT
     cities.id,
     cities.name,
-    states.name AS state_name
+    states.name AS name 
 FROM cities
     JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
