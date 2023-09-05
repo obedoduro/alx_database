6e3b625d1bc0e9518d3d5f44674a3a4ac9f67184
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-09-05 14:14:32 +0000

    read user

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,26 +1,17 @@
+-- Check if the table exists (always a name)
+
 -- Create the database if it doesn't exist
 CREATE DATABASE
-IF NOT EXISTS hbtn_test_db_3;
+IF NOT EXISTS hbtn_test_db_4;
 
 -- Switch to the database
-USE hbtn_test_db_3;
+USE hbtn_test_db_4;
 
 -- Create the table if it doesn't exist
 CREATE TABLE
 IF NOT EXISTS force_name
 (
-    -- id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL DEFAULT 1,
     name VARCHAR
-(256) NOT NULL,
---PRIMARY KEY(id)
+(256)
 );
-
--- Insert sample data into the table
--- INSERT INTO force_name
---     (name)
--- VALUES
---     ('Holberton School'),
---     ('Python is cool'),
---     ('Holberton'),
---     ('School'),
---     ('C is fun');
