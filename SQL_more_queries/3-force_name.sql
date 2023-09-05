b551708c2ed5134b18c888934a76623244e08512
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-09-05 11:30:57 +0000

    always name

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -9,9 +9,12 @@ USE hbtn_test_db_3;
 CREATE TABLE
 IF NOT EXISTS force_name
 (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT
+NOT NULL AUTO_INCREMENT,
     name VARCHAR
-(256) NOT NULL
+(256) NOT NULL,
+PRIMARY KEY
+(id)
 );
 
 -- Insert sample data into the table
