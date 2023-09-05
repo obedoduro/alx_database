210f92268a01f0a45e1f7e5448dbbb94c4ae8947
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-09-05 14:31:55 +0000

    unique id

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -6,7 +6,7 @@ USE hbtn_test_db_5;
 
 CREATE TABLE
 IF NOT EXISTS unique_id (
-   id INT NOT NULL DEFAULT 1,
-    name VARCHAR(256)
+    id INT NOT NULL DEFAULT 1,
+    name VARCHAR(256),
+    UNIQUE KEY id_unique (id)
 );
-
