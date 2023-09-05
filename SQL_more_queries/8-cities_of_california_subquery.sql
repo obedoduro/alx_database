998d84178e65d97f0248893c42a04ef80cca3d85
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-09-05 20:15:48 +0000

    cities of califonia

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -43,4 +43,8 @@ IF NOT EXISTS cities
 
 -- List all the cities of California
 SELECT cities.id, cities.name
-FROM cities;
+FROM cities
+-- WHERE cities.state_id = (SELECT id
+-- FROM states
+-- WHERE name = 'California')
+ORDER BY cities.id ASC;
