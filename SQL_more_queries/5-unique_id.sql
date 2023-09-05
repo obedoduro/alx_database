d7078c35fc55fe5d749dd424623b7e5f5e90a5b0
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-09-05 14:36:18 +0000

    unique id

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,12 +1,17 @@
---creating database for unique id
-CREATE DATABASE 
-IF  NOT EXISTS hbtn_test_db_5;
+-- Create the database if it doesn't exist
+CREATE DATABASE
+IF NOT EXISTS hbtn_test_db_5;
 
-USE hbtn_test_db_5; 
+-- Switch to the database
+USE hbtn_test_db_5;
 
+-- Create the table if it doesn't exist
 CREATE TABLE
-IF NOT EXISTS unique_id (
+IF NOT EXISTS unique_id
+(
     id INT NOT NULL DEFAULT 1,
-    name VARCHAR(256),
-    UNIQUE KEY id_unique (id)
+    name VARCHAR
+(256),
+    UNIQUE KEY id_unique
+(id)
 );
