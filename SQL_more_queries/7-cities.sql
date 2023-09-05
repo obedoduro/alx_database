e52304e990ed24f39e0727ee138023d20fb475a6
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-09-05 15:20:49 +0000

    cities table

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -5,13 +5,12 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 
 -- Creating table cities with unique id and foreign key state_id
-CREATE TABLE IF NOT EXISTS cities(
-    id INT AUTO_INCREMENT NOT NULL,
+CREATE TABLE IF NOT EXISTS cities (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     state_id INT NOT NULL,
-
-
-    FOREIGN KEY foreign_id (state_id), 
-    UNIQUE KEY unique_id (id)
+    FOREIGN KEY (state_id) REFERENCES states (id),
+    name VARCHAR(256) NOT NULL
+  
 );
 
 
