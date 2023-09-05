6ee756750a029628016bb7645918a55f0b4a74eb
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-09-05 09:18:45 +0000

    always name

---
+++ SQL_more_queries/3-force_name.sql
@@ -0,0 +1,11 @@
+-- Check if the table exists (always a name)
+USE your_database_name;
+-- Replace 'your_database_name' with the actual database name
+
+-- Create the table if it doesn't exist
+CREATE TABLE
+IF NOT EXISTS force_name
+(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
