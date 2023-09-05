493e531f32ecf3da146fa3d271a4c6346e797f60
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-09-05 14:26:13 +0000

    unique id

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,8 +1,8 @@
 --creating database for unique id
 CREATE DATABASE 
-IF  NOT EXISTS hbtn_test_db_4;
+IF  NOT EXISTS hbtn_test_db_5;
 
-USE hbtn_test_db_4; 
+USE hbtn_test_db_5; 
 
 CREATE TABLE
 IF NOT EXISTS unique_id (
