3210a0e5b8084f9d2c4b947adc1c6892372996c5
Obed Oduro <oduroadjeiobed16@gmail.com>
2024-02-07 13:09:24 +0000

    sql database

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,22 +1,7 @@
--- Save this script as create_second_table.sql
-
-USE hbtn_0c_0;
-
--- Attempt to create the table (will fail if the table already exists)
-CREATE TABLE IF NOT EXISTS second_table (
-    id INT,
-    name VARCHAR(256),
-    score INT
-);
-
--- Ignore the error if the table already exists
-DO NOTHING;
-
--- Insert multiple rows into the second_table
-INSERT INTO second_table (id, name, score) VALUES
-    (1, 'John', 10),
-    (2, 'Alex', 3),
-    (3, 'Bob', 14),
-    (4, 'George', 8);
-
-Print second_table;
+-- Creates and fills a table second_table with attributes id, name and score in my MySQL server with multiple rows.
+CREATE TABLE
+IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table (id, name, score) VALUES(1, "John", 10);
+INSERT INTO second_table (id,name, score) VALUES(2, "Alex", 3);
+INSERT INTO second_table (id,name, score) VALUES(3, "Bob", 14);
+INSERT INTO second_table (id,name, score) VALUES(4, "George", 8);
