8d9613f72bac23deb9c636b26cb6d2b967e71d02
Obed Oduro <oduroadjeiobed16@gmail.com>
2024-02-07 12:59:11 +0000

    sql database

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,23 +1,3 @@
--- Create the database if it doesn't exist
-CREATE DATABASE
-IF NOT EXISTS hbtn_0c_0;
-
-
-
--- Save this script as list_first_table_rows.sql
-USE hbtn_0c_0;
-
-
--- Create the table if it doesn't exist
-CREATE TABLE
-IF NOT EXISTS first_table
-(
-  id int ,
-  name varchar
-(128) 
- );
-
--- fetch the row of the table
+-- Lists all rows of the table first_table in my MySQL server.
 SELECT *
 FROM first_table;
-
